6f1d77ef152deb37509e7eede0b7bb75036003d6
Björn Martensen <bjoern.martensen@gmail.com>
2017-07-04 10:52:53 +0200

    add more infos to users

--- index.js
+++ index.js
@@ -96,7 +96,7 @@ restService.post('/info', function (req, res) {
 
                 var users_output = users.length > 1
                     ? users.slice(0, -1).join(', ') + ' und ' + users.slice(-1)
-                    : users[0];
+                    : users[0].first_name  + ' ' + users[0].last_name;
 
                 speech = users_output + users_output.length > 1 ? ' sind ' : ' ist ' + 'zuständig für ' + client;
                 return generateResponse(res, speech);
