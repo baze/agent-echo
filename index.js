8a55687dd01fa24e19d656f405325ec4f52ea0a1
Björn Martensen <bjoern.martensen@gmail.com>
2017-07-05 14:51:20 +0200

    functions nicht auslagern

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ const WPAPI = require('wpapi');
 var moment = require('moment');
 moment.locale('de');
 
-require('./functions');
-
 const restService = express();
 
 restService.use(bodyParser.urlencoded({
@@ -236,3 +234,47 @@ restService.get('/ews', function (req, res) {
 restService.listen((process.env.PORT || 8000), function () {
     console.log("Server up and listening");
 });
+
+function getInfoForUsername(username) {
+    const _users = require('./users.json');
+
+    for (var i = 0, len = _users.length; i < len; i++) {
+
+        var user = _users[i];
+
+        if (user.username == username) {
+            console.log(user);
+            return user;
+        }
+    }
+}
+
+function getUsersForClient(client) {
+    const _users = require('./users.json');
+    var users = [];
+
+    for (var i = 0, len = _users.length; i < len; i++) {
+
+        var user = _users[i];
+
+        if (user.clients) {
+            var clients = user.clients;
+
+            for (var j = 0, len2 = clients.length; j < len2; j++) {
+                if (clients[j] == client) {
+                    users.push(user);
+                }
+            }
+        }
+    }
+
+    return users;
+}
+
+function generateResponse(res, speech) {
+    return res.json({
+        speech: speech,
+        displayText: speech,
+        source: 'webhook-echo-sample'
+    });
+}
