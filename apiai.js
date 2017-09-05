6032fbe9971744df3e17dbf247680f018850121a
Björn Martensen <bjoern.martensen@gmail.com>
2017-09-05 17:14:29 +0200

    refactor

--- apiai.js
+++ apiai.js
@@ -1,6 +1,13 @@
 'use strict';
 
-var restService = require('./restService');
+const express = require('express');
+const bodyParser = require('body-parser');
+const restService = express();
+
+restService.use(bodyParser.json());
+restService.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 const WPAPI = require('wpapi');
 const moment = require('moment');
@@ -462,4 +469,8 @@ restService.post('/helga', function (req, res) {
             break;
     }
 
+});
+
+restService.listen((process.env.PORT || 8000), function () {
+    console.log("Server up and listening");
 });
