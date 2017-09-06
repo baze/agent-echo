a3b18f1663810a5c29a37343bbc5d8b6cd92b002
Björn Martensen <bjoern.martensen@gmail.com>
2017-09-06 09:37:49 +0200

    refactor

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 'use strict';
 
+var restService = require('./restService');
+
 // require('./apiai');
 require('./alexa');
+
+restService.listen((process.env.PORT || 8000), function () {
+    console.log("Server up and listening");
+});
