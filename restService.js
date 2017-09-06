ab392688beefb17a12b0148a89180fbe4de717fc
Björn Martensen <bjoern.martensen@gmail.com>
2017-09-06 15:13:12 +0200

    refactor

--- restService.js
+++ restService.js
@@ -9,8 +9,8 @@ restService.use(bodyParser.urlencoded({
     extended: true
 }));
 
-restService.listen((process.env.PORT || 8000), function () {
+/*restService.listen((process.env.PORT || 8000), function () {
     console.log("Server up and listening");
-});
+});*/
 
 module.exports = restService;
