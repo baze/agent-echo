92df2016616fe7c08923d480f7c994cc6281f3c7
Björn Martensen <bjoern.martensen@gmail.com>
2017-09-07 09:57:53 +0200

    refactor

--- alexa-skills.js
+++ alexa-skills.js
@@ -15,15 +15,14 @@ module.exports = function(options) {
     // app.post(route, jsonParser, RequestValidator, function(req, res) {
     app.post(route, function(req, res) {
 
-        console.log(req.body);
-
         if(req.body.session.application.applicationId == appId || !appId.length) {
-
-            console.log(req.body.request.type);
 
             switch(req.body.request.type) {
 
                 case "LaunchRequest":
+
+                    console.log(launchCallback);
+
                     if(launchCallback)
                         launchCallback(req, res);
                     else
