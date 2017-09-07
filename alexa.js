a8a30a7464a2b7470a581dd2fcc0e09938f66195
Björn Martensen <bjoern.martensen@gmail.com>
2017-09-07 08:53:24 +0200

    refactor

--- alexa.js
+++ alexa.js
@@ -10,6 +10,8 @@ var alexa = new AlexaSkills({
     applicationId: "amzn1.ask.skill.17e64ff1-708e-432e-add3-f925579d1938" // optional, but recommended. If you do not set this leave it blank
 });
 
+console.log(AlexaSkills);
+console.log(new AlexaSkills());
 console.log(alexa);
 
 const apiai = require('apiai');
